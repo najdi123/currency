caa599e7bdef69fb98a4586e92a7ed04ecd264ec
monire91 <aglida1370@gmail.com>
2025-10-21 05:46:09 -0500

    trying to solve cors error

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -6,10 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable CORS for frontend integration
+  // app.enableCors({
+  //   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  //   credentials: true,
+  // });
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    credentials: true,
-  });
+  origin: ['http://46.34.163.223', 'http://localhost:3000'],
+  credentials: true,
+});
 
   // Enable validation globally
   app.useGlobalPipes(new ValidationPipe({
